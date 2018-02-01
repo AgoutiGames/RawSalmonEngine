e96dfdaba4e5ad22c4ee40c2c62027883c2c1480
VectorWolf <mr.joe63@googlemail.com>
2018-02-01 21:34:35 +0100

    Added a header licensing comment

--- source/game_types.hpp
+++ source/game_types.hpp
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2017-2018 Agouti Games Team (see the AUTHORS file)
+ *
+ * This file is part of the RawSalmonEngine.
+ *
+ * The RawSalmonEngine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The RawSalmonEngine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the RawSalmonEngine.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef GAME_TYPES_HPP_INCLUDED
 #define GAME_TYPES_HPP_INCLUDED
 #include <string>
