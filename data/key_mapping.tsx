0c0caf0c05009f6a08530a6d7d848b9592dedc45
VectorWolf <mr.joe63@googlemail.com>
2018-03-21 23:09:13 +0100

    Add key configuration template entries

--- data/key_mapping.tsx
+++ data/key_mapping.tsx
@@ -1,4 +1,300 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="Key_Mapping" tilewidth="60" tileheight="60" tilecount="60" columns="12">
  <image source="tilesets/keyboard.png" width="720" height="300"/>
+ <tile id="0" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="1"/>
+  </properties>
+ </tile>
+ <tile id="1" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="2"/>
+  </properties>
+ </tile>
+ <tile id="2" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="3"/>
+  </properties>
+ </tile>
+ <tile id="3" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="4"/>
+  </properties>
+ </tile>
+ <tile id="4" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="5"/>
+  </properties>
+ </tile>
+ <tile id="5" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="6"/>
+  </properties>
+ </tile>
+ <tile id="6" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="7"/>
+  </properties>
+ </tile>
+ <tile id="7" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="8"/>
+  </properties>
+ </tile>
+ <tile id="8" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="9"/>
+  </properties>
+ </tile>
+ <tile id="9" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="0"/>
+  </properties>
+ </tile>
+ <tile id="10" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="+"/>
+  </properties>
+ </tile>
+ <tile id="11" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="-"/>
+  </properties>
+ </tile>
+ <tile id="12" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="Escape"/>
+  </properties>
+ </tile>
+ <tile id="13" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="Q"/>
+  </properties>
+ </tile>
+ <tile id="14" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="W"/>
+  </properties>
+ </tile>
+ <tile id="15" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="E"/>
+  </properties>
+ </tile>
+ <tile id="16" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="R"/>
+  </properties>
+ </tile>
+ <tile id="17" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="T"/>
+  </properties>
+ </tile>
+ <tile id="18" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="Y"/>
+  </properties>
+ </tile>
+ <tile id="19" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="U"/>
+  </properties>
+ </tile>
+ <tile id="20" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="I"/>
+  </properties>
+ </tile>
+ <tile id="21" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="O"/>
+  </properties>
+ </tile>
+ <tile id="22" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="P"/>
+  </properties>
+ </tile>
+ <tile id="23" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="Backspace"/>
+  </properties>
+ </tile>
+ <tile id="24" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="Tab"/>
+  </properties>
+ </tile>
+ <tile id="25" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="A"/>
+  </properties>
+ </tile>
+ <tile id="26" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="S"/>
+  </properties>
+ </tile>
+ <tile id="27" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="D"/>
+  </properties>
+ </tile>
+ <tile id="28" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="F"/>
+  </properties>
+ </tile>
+ <tile id="29" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="G"/>
+  </properties>
+ </tile>
+ <tile id="30" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="H"/>
+  </properties>
+ </tile>
+ <tile id="31" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="J"/>
+  </properties>
+ </tile>
+ <tile id="32" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="K"/>
+  </properties>
+ </tile>
+ <tile id="33" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="L"/>
+  </properties>
+ </tile>
+ <tile id="34" type="KEY_MAPPING"/>
+ <tile id="35" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="Return"/>
+  </properties>
+ </tile>
+ <tile id="36" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="CapsLock"/>
+  </properties>
+ </tile>
+ <tile id="37" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="Z"/>
+  </properties>
+ </tile>
+ <tile id="38" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="X"/>
+  </properties>
+ </tile>
+ <tile id="39" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="C"/>
+  </properties>
+ </tile>
+ <tile id="40" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="V"/>
+  </properties>
+ </tile>
+ <tile id="41" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="B"/>
+  </properties>
+ </tile>
+ <tile id="42" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="N"/>
+  </properties>
+ </tile>
+ <tile id="43" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="M"/>
+  </properties>
+ </tile>
+ <tile id="44" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="&lt;"/>
+  </properties>
+ </tile>
+ <tile id="45" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="&gt;"/>
+  </properties>
+ </tile>
+ <tile id="46" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="Up"/>
+  </properties>
+ </tile>
+ <tile id="47" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="?"/>
+  </properties>
+ </tile>
+ <tile id="48" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="Left Shift"/>
+  </properties>
+ </tile>
+ <tile id="49" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="Left Ctrl"/>
+  </properties>
+ </tile>
+ <tile id="50" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="Left Alt"/>
+  </properties>
+ </tile>
+ <tile id="51" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="Space"/>
+  </properties>
+ </tile>
+ <tile id="52" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="Right Alt"/>
+  </properties>
+ </tile>
+ <tile id="53" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="Right Ctrl"/>
+  </properties>
+ </tile>
+ <tile id="54" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="Right Shift"/>
+  </properties>
+ </tile>
+ <tile id="55" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="/"/>
+  </properties>
+ </tile>
+ <tile id="56" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="#"/>
+  </properties>
+ </tile>
+ <tile id="57" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="Left"/>
+  </properties>
+ </tile>
+ <tile id="58" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="Down"/>
+  </properties>
+ </tile>
+ <tile id="59" type="KEY_MAPPING">
+  <properties>
+   <property name="KEYPRESS" value="Right"/>
+  </properties>
+ </tile>
 </tileset>
