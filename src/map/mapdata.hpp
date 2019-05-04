013256f518585d2f64e2f1c0fe3f202243a580f5
VectorWolf <mr.joe63@googlemail.com>
2019-05-04 12:21:52 +0200

    Add TilesetCollection dummy class

--- src/map/mapdata.hpp
+++ src/map/mapdata.hpp
@@ -47,13 +47,9 @@ class MapData {
         bool render() const;
         void update();
 
-        unsigned get_overhang(Direction dir) const;
         SDL_Renderer* get_renderer() const {return *mpp_renderer;} ///< Return pointer to the SDL_Renderer
         std::string get_file_path() const {return m_base_path;} ///< Return path to the .tmx map file location
-        Uint16 get_gid(Tile* tile)  const;
-        Tile* get_tile(Uint16 tile_id) const;
-        unsigned get_tile_h() const {return m_tile_h;} ///< Return base tile height
-        unsigned get_tile_w() const {return m_tile_w;} ///< Return base tile width
+
         unsigned get_w() const {return m_width * m_tile_w;} ///< Return map width in pixels
         unsigned get_h() const {return m_height * m_tile_h;} ///< Return map height in pixels
         DataBlock& get_data() {return m_data;}
@@ -73,6 +69,17 @@ class MapData {
         void add_actor_animation(std::string name, AnimationType anim, Direction dir, Tile* tile);
         bool add_actor_hitbox(std::string actor, std::string hitbox, const SDL_Rect& rect);
 
+
+
+
+        unsigned get_tile_h() const {return m_tile_h;} ///< Return base tile height
+        unsigned get_tile_w() const {return m_tile_w;} ///< Return base tile width
+
+        unsigned get_overhang(Direction dir) const;
+
+        Uint16 get_gid(Tile* tile)  const;
+        Tile* get_tile(Uint16 tile_id) const;
+
         tinyxml2::XMLError parse_tiles_from_tileset(tinyxml2::XMLElement* source, unsigned first_gid);
         bool register_tile(Tile* tile, unsigned gid);
         void set_tile_animated(unsigned gid);
@@ -83,6 +90,9 @@ class MapData {
 
         bool render(Uint16 tile_id, int x, int y) const;
         bool render(Uint16 tile_id, SDL_Rect& dest) const;
+
+
+
 
         tinyxml2::XMLError parse_actor_properties(tinyxml2::XMLElement* source, float& speed, Direction& dir, std::map<Response, ActorEvent*>& resp);
 
@@ -96,8 +106,6 @@ class MapData {
         tinyxml2::XMLDocument m_mapfile{true, tinyxml2::COLLAPSE_WHITESPACE}; ///< Contains the .tmx map file
 
         std::string m_base_path = "../data/"; ///< Path to folder where .tmx map files are
-        unsigned m_tile_w; // The tile dimensions in pixels
-        unsigned m_tile_h;
         unsigned m_width;  // The map dimensions in tiles
         unsigned m_height;
         SDL_Color m_bg_color;
@@ -105,6 +113,13 @@ class MapData {
         DataBlock m_data; ///< This holds custom user values by string
 
         Camera m_camera;
+
+        std::vector<Layer> m_layers; ///< Contains all used layers
+
+
+
+        unsigned m_tile_w; // The tile dimensions in pixels
+        unsigned m_tile_h;
                                //                         | |
         void write_overhang(); // sets the 4 values below v v
         unsigned m_up_overhang = 0;
@@ -113,11 +128,13 @@ class MapData {
         unsigned m_right_overhang = 0;
 
         std::vector<Tileset> m_tilesets; ///< Contains all used Tilesets
-        std::vector<Layer> m_layers; ///< Contains all used layers
 
         std::vector<Tile*> mp_tiles;      ///< List of pointers to all tiles in order
         std::vector<Uint16> m_anim_tiles; ///< List of ids of all animated tiles
         Tile m_empty_tile; ///< A placeholder which currently has no use except matching gids (this is tile 0)
+
+
+
 
         std::map<std::string, ActorTemplate> m_templates; ///< List of all actor templates by name
         std::map<Uint16, std::string> m_gid_to_temp_name; ///< List of actor template names by global tile id
---
+++ src/map/tileset_collection.hpp
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2017-2018 Agouti Games Team (see the AUTHORS file)
+ *
+ * This file is part of the RawSalmonEngine.
+ *
+ * The RawSalmonEngine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The RawSalmonEngine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the RawSalmonEngine.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef TILESET_COLLECTION_HPP_INCLUDED
+#define TILESET_COLLECTION_HPP_INCLUDED
+
+class TilesetCollection{
+    public:
+
+
+        unsigned get_tile_h() const {return m_tile_h;} ///< Return base tile height
+        unsigned get_tile_w() const {return m_tile_w;} ///< Return base tile width
+
+        unsigned get_overhang(Direction dir) const;
+
+        Uint16 get_gid(Tile* tile)  const;
+        Tile* get_tile(Uint16 tile_id) const;
+
+        tinyxml2::XMLError parse_tiles_from_tileset(tinyxml2::XMLElement* source, unsigned first_gid);
+        bool register_tile(Tile* tile, unsigned gid);
+        void set_tile_animated(unsigned gid);
+        void set_tile_animated(Tile* tile);
+
+        void init_anim_tiles();
+        void push_all_anim();
+
+        bool render(Uint16 tile_id, int x, int y) const;
+        bool render(Uint16 tile_id, SDL_Rect& dest) const;
+
+    private:
+        unsigned m_tile_w; // The tile dimensions in pixels
+        unsigned m_tile_h;
+
+        void write_overhang(); // sets the 4 values below v v
+        unsigned m_up_overhang = 0;
+        unsigned m_down_overhang = 0;
+        unsigned m_left_overhang = 0;
+        unsigned m_right_overhang = 0;
+
+        std::vector<Tileset> m_tilesets; ///< Contains all used Tilesets
+
+        std::vector<Tile*> mp_tiles;      ///< List of pointers to all tiles in order
+        std::vector<Uint16> m_anim_tiles; ///< List of ids of all animated tiles
+        Tile m_empty_tile; ///< A placeholder which currently has no use except matching gids (this is tile 0)
+};
+
+
+#endif // TILESET_COLLECTION_HPP_INCLUDED
