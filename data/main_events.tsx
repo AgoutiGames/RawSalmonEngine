505cbb3448d50d95fe8703488068cc18e3b7fc62
VectorWolf <mr.joe63@googlemail.com>
2019-07-02 07:07:33 +0200

    Fix render order issue when x-staggering isometric - Extract method of map clip - Seperate clip into three independent functions

--- data/main_events.tsx
+++ data/main_events.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="main_events" tilewidth="60" tileheight="60" tilecount="100" columns="10">
- <image source="environment/logic/symbol_numbers.png" width="600" height="600"/>
+ <image source="tilesets/symbol_numbers.png" width="600" height="600"/>
  <tile id="0" type="GeChangeMap">
   <properties>
    <property name="NAME" value="load_map"/>
--- src/map/map_layer.cpp
+++ src/map/map_layer.cpp
@@ -127,101 +127,40 @@ bool MapLayer::render(const Camera& camera) const {
     return success;
 }
 
+std::vector< std::tuple<Uint16, int, int> > MapLayer::clip(SDL_Rect rect) const {
+    const TileLayout layout = m_layer_collection->get_base_map().get_tile_layout();
+    if(layout.orientation == "orthogonal") {
+        return clip_ortho(rect);
+    }
+    else if(layout.stagger_axis_y) {
+        return clip_y_stagger(rect);
+    }
+    else {
+        return clip_x_stagger(rect);
+    }
+}
+
 /**
  * @brief Generates a vector of tiles information for tiles possibly bounding with a rect
  * @param rect The rectangular space which the tiles are bounding with
  * @return A vector of TileId, x-coord(relative to recto origin), y-coord(relative to rect origin), tuples
  */
-std::vector< std::tuple<Uint16, int, int> > MapLayer::clip(const SDL_Rect& rect) const {
+std::vector< std::tuple<Uint16, int, int> > MapLayer::clip_ortho(SDL_Rect rect) const {
 
-    const TileLayout layout = m_layer_collection->get_base_map().get_tile_layout();
+    int tile_w = static_cast<int>(m_ts_collection->get_tile_w());
+    int tile_h = static_cast<int>(m_ts_collection->get_tile_h());
 
-    bool staggered = false;
-    if(layout.orientation != "orthogonal") {
-        staggered = true;
-    }
-
-    int base_tile_w = static_cast<int>(m_ts_collection->get_tile_w());
-    int base_tile_h = static_cast<int>(m_ts_collection->get_tile_h());
-
-    int tile_w = base_tile_w;
-    int tile_h = base_tile_h;
-
-    if(staggered) {
-        if(layout.stagger_axis_y) {
-            tile_h /= 2;
-            tile_h += layout.hexsidelength / 2;
-        }
-        else {
-            tile_w /= 2;
-            tile_w += layout.hexsidelength / 2;
-        }
-    }
-
-    // Apply the layer offset
-    int x_camera = rect.x - m_offset_x;
-    int y_camera = rect.y - m_offset_y;
-
-    // Horizontal range of tiles to render
-    int x_tile_from = x_camera / tile_w;
-    int x_tile_to = (x_camera + rect.w) / tile_w;
-
-    // Horizontal pixel offset to full tile
-    int x_tile_offset = x_camera % tile_w;
-
-    // Vertical range of tiles to render
-    int y_tile_from = y_camera / tile_h;
-    int y_tile_to = (y_camera + rect.h) / tile_h;
-
-    // Vertical pixel offset to full tile
-    int y_tile_offset = y_camera % tile_h;
-
-    // Calculate tile offset by pixel basis
-    float left_oh = m_ts_collection->get_overhang(Direction::left);
-    float right_oh = m_ts_collection->get_overhang(Direction::right);
-    float up_oh = m_ts_collection->get_overhang(Direction::up);
-    float down_oh = m_ts_collection->get_overhang(Direction::down);
-
-    if(staggered) {
-        left_oh += base_tile_w / 2;
-        up_oh += base_tile_h / 2;
-    }
-
-    left_oh = ceil(left_oh / tile_w);
-    right_oh = ceil(right_oh / tile_w);
-    up_oh = ceil(up_oh / tile_h);
-    down_oh = ceil(down_oh / tile_h);
-
-    // Apply the margin which makes up for oversized tiles and tileset offset
-    x_tile_from -= left_oh;
-    x_tile_to += right_oh;
-    y_tile_from -= up_oh;
-    y_tile_to += down_oh;
-
-    // Pixel perfect position of the first upper left tile
-    int x_start = -x_tile_offset - (left_oh * tile_w);
-    int y_start = -y_tile_offset - (up_oh * tile_h);
-
-    int x = x_start;
-    int y = y_start;
-
-    int x_step = tile_w;
-    int y_step = tile_h;
+    int x_tile_from, x_tile_to, y_tile_from, y_tile_to, x_start, y_start;
+    calc_tile_range(rect, tile_w, tile_h, x_tile_from, x_tile_to, y_tile_from, y_tile_to, x_start, y_start);
 
     std::vector< std::tuple<Uint16, int, int> > tiles;
     tiles.reserve((y_tile_to - y_tile_from + 1) * (x_tile_to - x_tile_from + 1));
 
+    int y = y_start;
     // Iterates through vertical rows tile by tile
     for(int i_y_tile = y_tile_from; i_y_tile <= y_tile_to; i_y_tile++) {
         // Reset horizontal tile position
-        x = x_start;
-        if(staggered) {
-            if(layout.stagger_axis_y) {
-                if((!layout.stagger_index_odd && i_y_tile % 2 == 0) || (layout.stagger_index_odd && i_y_tile % 2 != 0)) {
-                    x = x_start + (tile_w / 2);
-                }
-            }
-        }
+        int x = x_start;
         // Skips vertical rows if position is off map/layer
         if(i_y_tile >= 0 && i_y_tile < static_cast<int>(m_height)) {
 
@@ -235,25 +174,196 @@ std::vector< std::tuple<Uint16, int, int> > MapLayer::clip(const SDL_Rect& rect)
                     Uint16 tile_id = m_map_grid[i_y_tile][i_x_tile];
                     // Scrap empty tiles!
                     if(tile_id != 0) {
-                        int temp_y = y;
-
-                        if(staggered) {
-                            if(!layout.stagger_axis_y) {
-                                if((!layout.stagger_index_odd && i_x_tile % 2 == 0) || (layout.stagger_index_odd && i_x_tile % 2 != 0)) {
-                                    temp_y = y + (tile_h / 2);
-                                }
-                            }
-                        }
-                        tiles.emplace_back(tile_id, x, temp_y);
+                        tiles.emplace_back(tile_id, x, y);
                     }
                 }
                 // Move to next horizontal tile position
-                x += x_step;
+                x += tile_w;
             }
         }
         // Move to next vertical tile position
-        y += y_step;
+        y += tile_h;
     }
     return tiles;
 }
 
+/**
+ * @brief Generates a vector of tiles information for tiles possibly bounding with a rect
+ * @param rect The rectangular space which the tiles are bounding with
+ * @return A vector of TileId, x-coord(relative to recto origin), y-coord(relative to rect origin), tuples
+ */
+std::vector< std::tuple<Uint16, int, int> > MapLayer::clip_y_stagger(SDL_Rect rect) const {
+
+    const TileLayout layout = m_layer_collection->get_base_map().get_tile_layout();
+
+    int tile_w = static_cast<int>(m_ts_collection->get_tile_w());
+    int tile_h = static_cast<int>(m_ts_collection->get_tile_h());
+
+    // Conform to y_stagger
+    tile_h /= 2;
+    tile_h += layout.hexsidelength / 2;
+
+    int x_tile_from, x_tile_to, y_tile_from, y_tile_to, x_start, y_start;
+    calc_tile_range(rect, tile_w, tile_h, x_tile_from, x_tile_to, y_tile_from, y_tile_to, x_start, y_start);
+
+    std::vector< std::tuple<Uint16, int, int> > tiles;
+    tiles.reserve((y_tile_to - y_tile_from + 1) * (x_tile_to - x_tile_from + 1));
+
+    int y = y_start;
+    // Iterates through vertical rows tile by tile
+    for(int i_y_tile = y_tile_from; i_y_tile <= y_tile_to; i_y_tile++) {
+        // Reset horizontal tile position
+        int x = x_start;
+        if((!layout.stagger_index_odd && i_y_tile % 2 == 0) || (layout.stagger_index_odd && i_y_tile % 2 != 0)) {
+            x = x_start + (tile_w / 2);
+        }
+
+        // Skips vertical rows if position is off map/layer
+        if(i_y_tile >= 0 && i_y_tile < static_cast<int>(m_height)) {
+
+            // Iterates through horizontal rows tile by tile
+            for(int i_x_tile = x_tile_from; i_x_tile <= x_tile_to; i_x_tile++) {
+
+                // Skips horizontal rows if position is off map/layer
+                if(i_x_tile >= 0 && i_x_tile < static_cast<int>(m_width)) {
+
+                    // Get tile id from map layer data and draw at current position if tile_id is not 0
+                    Uint16 tile_id = m_map_grid[i_y_tile][i_x_tile];
+                    // Scrap empty tiles!
+                    if(tile_id != 0) {
+                        tiles.emplace_back(tile_id, x, y);
+                    }
+                }
+                // Move to next horizontal tile position
+                x += tile_w;
+            }
+        }
+        // Move to next vertical tile position
+        y += tile_h;
+    }
+    return tiles;
+}
+
+/**
+ * @brief Generates a vector of tiles information for tiles possibly bounding with a rect
+ * @param rect The rectangular space which the tiles are bounding with
+ * @return A vector of TileId, x-coord(relative to recto origin), y-coord(relative to rect origin), tuples
+ */
+std::vector< std::tuple<Uint16, int, int> > MapLayer::clip_x_stagger(SDL_Rect rect) const {
+
+    const TileLayout layout = m_layer_collection->get_base_map().get_tile_layout();
+
+    int tile_w = static_cast<int>(m_ts_collection->get_tile_w());
+    int tile_h = static_cast<int>(m_ts_collection->get_tile_h());
+
+    // Conform to x stagger
+    tile_w /= 2;
+    tile_w += layout.hexsidelength / 2;
+
+    int x_tile_from, x_tile_to, y_tile_from, y_tile_to, x_start, y_start;
+    calc_tile_range(rect, tile_w, tile_h, x_tile_from, x_tile_to, y_tile_from, y_tile_to, x_start, y_start);
+
+    // Per row fetch every second tile
+    int x_step = tile_w * 2;
+    // Split each row into two
+    int y_step = tile_h / 2;
+
+    std::vector< std::tuple<Uint16, int, int> > tiles;
+    tiles.reserve((y_tile_to - y_tile_from + 1) * (x_tile_to - x_tile_from + 1));
+
+    // Determine if first half row starts odd or even
+    int odd_even = 0;
+    if((!layout.stagger_index_odd && x_tile_from % 2 == 0) || (layout.stagger_index_odd && x_tile_from % 2 != 0)) {
+        odd_even = 1;
+    }
+
+    int y = y_start;
+    // Iterates through vertical rows tile by tile
+    for(int i_y_tile = y_tile_from; i_y_tile <= y_tile_to; i_y_tile++) {
+        // Skips vertical rows if position is off map/layer
+        if(i_y_tile >= 0 && i_y_tile < static_cast<int>(m_height)) {
+            // Fetch row in two passes for correct rendering order
+            for(int i_odd_even = 0; i_odd_even < 2; i_odd_even++) {
+                // Reset horizontal tile position
+                int x = x_start;
+                int x_offset = (i_odd_even + odd_even) % 2;
+                x += x_offset * tile_w;
+
+                // Iterates through horizontal rows tile by tile
+                for(int i_x_tile = x_tile_from + x_offset; i_x_tile <= x_tile_to; i_x_tile += 2) {
+
+                    // Skips horizontal rows if position is off map/layer
+                    if(i_x_tile >= 0 && i_x_tile < static_cast<int>(m_width)) {
+
+                        // Get tile id from map layer data and draw at current position if tile_id is not 0
+                        Uint16 tile_id = m_map_grid[i_y_tile][i_x_tile];
+                        // Scrap empty tiles!
+                        if(tile_id != 0) {
+                            tiles.emplace_back(tile_id, x, y);
+                        }
+                    }
+                    // Move to next horizontal tile position
+                    x += x_step;
+                }
+                // Move down half a row
+                y += y_step;
+            }
+            // Prevent stepping down twice
+            continue;
+        }
+        // Move down a full row
+        y += y_step * 2;
+    }
+    return tiles;
+}
+
+/// Calculate the range of tiles bounding with rect
+void MapLayer::calc_tile_range(SDL_Rect rect, int tile_w, int tile_h, int& x_from, int& x_to, int& y_from, int& y_to, int& x_start, int& y_start) const {
+
+    // Apply the layer offset
+    rect.x -= m_offset_x;
+    rect.y -= m_offset_y;
+
+    // Horizontal range of tiles to render
+    x_from = rect.x / tile_w;
+    x_to = (rect.x + rect.w) / tile_w;
+
+    // Horizontal pixel offset to full tile
+    int x_tile_offset = rect.x % tile_w;
+
+    // Vertical range of tiles to render
+    y_from = rect.y / tile_h;
+    y_to = (rect.y + rect.h) / tile_h;
+
+    // Vertical pixel offset to full tile
+    int y_tile_offset = rect.y % tile_h;
+
+    // Calculate tile offset by pixel basis
+    float left_oh = m_ts_collection->get_overhang(Direction::left);
+    float right_oh = m_ts_collection->get_overhang(Direction::right);
+    float up_oh = m_ts_collection->get_overhang(Direction::up);
+    float down_oh = m_ts_collection->get_overhang(Direction::down);
+
+    const TileLayout layout = m_layer_collection->get_base_map().get_tile_layout();
+    if(layout.orientation != "orthogonal") {
+        // Render half tile extra to show pointy tile borders
+        left_oh += static_cast<int>(m_ts_collection->get_tile_w()) / 2;
+        up_oh += static_cast<int>(m_ts_collection->get_tile_h()) / 2;
+    }
+
+    left_oh = ceil(left_oh / tile_w);
+    right_oh = ceil(right_oh / tile_w);
+    up_oh = ceil(up_oh / tile_h);
+    down_oh = ceil(down_oh / tile_h);
+
+    // Apply the margin which makes up for oversized tiles and tileset offset
+    x_from -= left_oh;
+    x_to += right_oh;
+    y_from -= up_oh;
+    y_to += down_oh;
+
+    // Pixel perfect position of the first upper left tile
+    x_start = -x_tile_offset - (left_oh * tile_w);
+    y_start = -y_tile_offset - (up_oh * tile_h);
+
+}
