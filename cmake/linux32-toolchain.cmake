5052e4cb3dbd2e746f81ef8372f7e11d421abd62
VectorWolf <mr.joe63@googlemail.com>
2019-07-04 18:32:45 +0200

    Add travis ci build matrix

---
+++ cmake/linux32-toolchain.cmake
@@ -0,0 +1,20 @@
+# the name of the target operating system
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR "i386")
+
+# which compilers to use for C and C++
+set(CMAKE_C_COMPILER gcc)
+set(CMAKE_C_FLAGS -m32)
+set(CMAKE_CXX_COMPILER g++)
+set(CMAKE_CXX_FLAGS -m32)
+
+# here is the target environment located
+# set(CMAKE_FIND_ROOT_PATH /usr/lib/i386-linux-gnu/)
+# ubuntu seems to find the libs no problem when i386/-m32 is specified
+
+# adjust the default behaviour of the FIND_XXX() commands:
+# search headers and libraries in the target environment, search
+# programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
