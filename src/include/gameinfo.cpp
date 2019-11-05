2e65de0c80b265c8cbc6f62f29b41a354a1f3375
VectorWolf <mr.joe63@googlemail.com>
2019-11-05 22:02:53 +0100

    Use pimpl idiom for gameinfo

---
+++ src/include/gameinfo.cpp
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2017-2019 Agouti Games Team (see the AUTHORS file)
+ *
+ * This file is part of the RawSalmonEngine.
+ *
+ * The RawSalmonEngine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The RawSalmonEngine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the RawSalmonEngine.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "include/gameinfo.hpp"
+
+#include "core/gameinfo.hpp"
+#include "actor/data_block.hpp"
+
+salmon::GameInfo::GameInfo(unsigned screen_w, unsigned screen_h) : m_impl{std::make_unique<::GameInfo>(screen_w,screen_h)} {}
+salmon::GameInfo::~GameInfo() {}
+
+bool salmon::GameInfo::init() {return m_impl->init();}
+void salmon::GameInfo::close() {return m_impl->close();}
+
+void salmon::GameInfo::render() {m_impl->render();}
+bool salmon::GameInfo::update() {return m_impl->update();}
+
+bool salmon::GameInfo::load_map(std::string mapfile, bool absolute) {return m_impl->load_map(mapfile, absolute);}
+void salmon::GameInfo::close_map() {m_impl->close_map();}
+
+DataBlock& salmon::GameInfo::get_data() {return m_impl->get_data();}
+FontManager& salmon::GameInfo::get_font_manager() {return m_impl->get_font_manager();}
---
+++ src/include/gameinfo.hpp
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2017-2019 Agouti Games Team (see the AUTHORS file)
+ *
+ * This file is part of the RawSalmonEngine.
+ *
+ * The RawSalmonEngine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The RawSalmonEngine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the RawSalmonEngine.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef GAMEINFO_LIB_HPP_INCLUDED
+#define GAMEINFO_LIB_HPP_INCLUDED
+
+#include <SDL.h>
+#include <string>
+#include <memory>
+
+class GameInfo;
+class FontManager;
+class DataBlock;
+
+namespace salmon {
+    class GameInfo{
+        public:
+            GameInfo(unsigned screen_w, unsigned screen_h);
+            ~GameInfo();
+
+            bool init();
+            void close();
+
+            void render();
+            bool update();
+
+            bool load_map(std::string mapfile, bool absolute = false);
+            void close_map();
+
+            DataBlock& get_data();
+            FontManager& get_font_manager();
+
+        private:
+            std::unique_ptr<::GameInfo> m_impl;
+    };
+}
+
+
+#endif // GAMEINFO_LIB_HPP_INCLUDED
