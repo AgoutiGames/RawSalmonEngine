4f8b000020bce8ec5ecc9acd921dec141f395675
VectorWolf <mr.joe63@googlemail.com>
2019-11-07 06:48:53 +0100

    Add actor, camera and map library reference prototypes

---
+++ src/include/actor_ref.hpp
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2017-2019 Agouti Games Team (see the AUTHORS file)
+ *
+ * This file is part of the RawSalmonEngine.
+ *
+ * The RawSalmonEngine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The RawSalmonEngine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the RawSalmonEngine.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef ACTOR_REF_HPP_INCLUDED
+#define ACTOR_REF_HPP_INCLUDED
+
+class Actor;
+
+namespace salmon {
+    class ActorRef {
+        public:
+            ActorRef(Actor& impl);
+        private:
+            Actor* m_impl;
+    };
+}
+
+#endif // ACTOR_REF_HPP_INCLUDED
