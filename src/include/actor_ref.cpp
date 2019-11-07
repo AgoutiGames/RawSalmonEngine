5850187b5f4118dea105a71c33852258a70147d2
VectorWolf <mr.joe63@googlemail.com>
2019-11-07 19:14:19 +0100

    Add basic actor reference implementation

--- src/include/actor_ref.cpp
+++ src/include/actor_ref.cpp
@@ -23,3 +23,26 @@
 using salmon::ActorRef;
 
 ActorRef::ActorRef(Actor& impl) : m_impl{&impl} {}
+
+bool ActorRef::animate(AnimationType anim, Direction dir, float speed) {return m_impl->animate(anim,dir,speed);}
+bool ActorRef::set_animation(AnimationType anim, Direction dir, int frame) {return m_impl->set_animation(anim,dir,frame);}
+salmon::AnimSignal ActorRef::animate_trigger(AnimationType anim, Direction dir, float speed) {return m_impl->animate_trigger(anim,dir,speed);}
+AnimationType ActorRef::get_animation() const {return m_impl->get_animation();}
+Direction ActorRef::get_direction() const {return m_impl->get_direction();}
+std::string ActorRef::get_name() const {return m_impl->get_name();}
+std::string ActorRef::get_type() const {return m_impl->get_type();}
+
+bool ActorRef::move(float x_factor, float y_factor, bool absolute) {return m_impl->move(x_factor, y_factor, absolute);}
+bool ActorRef::on_ground(Direction dir, int tolerance) const {return m_impl->on_ground(dir, tolerance);}
+
+double ActorRef::get_angle() const {return m_impl->get_angle();}
+void ActorRef::set_angle(double angle) {return m_impl->set_angle(angle);}
+
+float ActorRef::get_x() const {return m_impl->get_x();}
+float ActorRef::get_y() const {return m_impl->get_y();}
+unsigned ActorRef::get_w() const {return m_impl->get_w();}
+unsigned ActorRef::get_h() const {return m_impl->get_h();}
+int ActorRef::get_x_center() const {return m_impl->get_x_center();}
+int ActorRef::get_y_center() const {return m_impl->get_y_center();}
+
+salmon::DataBlockRef ActorRef::get_data() {return m_impl->get_data();}
--- src/include/actor_ref.hpp
+++ src/include/actor_ref.hpp
@@ -19,6 +19,11 @@
 #ifndef ACTOR_REF_HPP_INCLUDED
 #define ACTOR_REF_HPP_INCLUDED
 
+#include <string>
+
+#include "util/game_types.hpp"
+#include "./data_block_ref.hpp"
+
 class Actor;
 
 namespace salmon {
@@ -26,6 +31,30 @@ namespace salmon {
         friend class CameraRef;
         public:
             ActorRef(Actor& impl);
+
+            bool animate(AnimationType anim = AnimationType::current, Direction dir = Direction::current, float speed = 1.0);
+            bool set_animation(AnimationType anim = AnimationType::current, Direction dir = Direction::current, int frame = 0);
+            salmon::AnimSignal animate_trigger(AnimationType anim = AnimationType::current, Direction dir = Direction::current, float speed = 1.0);
+            AnimationType get_animation() const;
+            Direction get_direction() const;
+            std::string get_name() const;
+            std::string get_type() const;
+
+            bool move(float x_factor, float y_factor, bool absolute = false);
+            bool on_ground(Direction dir = Direction::down, int tolerance = 0) const;
+
+            double get_angle() const;
+            void set_angle(double angle);
+
+            float get_x() const;
+            float get_y() const;
+            unsigned get_w() const;
+            unsigned get_h() const;
+            int get_x_center() const;
+            int get_y_center() const;
+
+            DataBlockRef get_data();
+
         private:
             Actor* m_impl;
     };
