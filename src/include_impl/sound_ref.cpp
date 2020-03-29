781ffaa4b52115ccf4be4fa8b04aa07c1f4f93f4
VectorWolf <mr.joe63@googlemail.com>
2020-03-29 23:16:49 +0200

    Fix severe memory management bug of SoundRef leading to segfault

--- src/include_impl/sound_ref.cpp
+++ src/include_impl/sound_ref.cpp
@@ -27,20 +27,20 @@ SoundRef::SoundRef(SoundEffect& impl) : m_impl{new SoundEffect(impl)} {}
 
 SoundRef::SoundRef(const SoundRef& sound) : SoundRef(*sound.m_impl) {}
 SoundRef::SoundRef(SoundRef&& sound) : m_impl{sound.m_impl} {
-    sound.m_impl = nullptr;
+    sound.m_impl = SoundEffect::get_null_sound();
 }
 SoundRef& SoundRef::operator=(const SoundRef& sound) {
-    delete m_impl;
+    if(m_impl != SoundEffect::get_null_sound()) {delete m_impl;}
     m_impl = new SoundEffect(*sound.m_impl);
     return *this;
 }
 SoundRef& SoundRef::operator=(SoundRef&& sound) {
-    delete m_impl;
+    if(m_impl != SoundEffect::get_null_sound()) {delete m_impl;}
     m_impl = sound.m_impl;
-    sound.m_impl = nullptr;
+    sound.m_impl = SoundEffect::get_null_sound();
     return *this;
 }
-SoundRef::~SoundRef() {if(m_impl != nullptr) {delete m_impl;}}
+SoundRef::~SoundRef() {if(m_impl != SoundEffect::get_null_sound()) {delete m_impl;}}
 
 bool SoundRef::good() const {return m_impl->good();}
 void SoundRef::play(int loops, int length_ms) {m_impl->play(loops,length_ms);}
